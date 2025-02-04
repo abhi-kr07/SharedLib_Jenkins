e1530e2675bc3442e0d326e2c6b96a5e528e641a
abhi-kr07 <112303518+abhi-kr07@users.noreply.github.com>
2025-02-05 04:56:08 +0530

    Update sonarqube_codeanalysis.groovy

--- vars/sonarqube_codeanalysis.groovy
+++ vars/sonarqube_codeanalysis.groovy
@@ -1,6 +1,5 @@
 def call(String SonarServerName , String ProjectName , String ProjectKey ) {
     withSonarQubeEnv("${SonarServerName}") {
-        sh ''' ${SCANNER_HOME}/bin/sonar_scanner -Dsonar.projectName=${ProjectName} \
-        -Dsonar.projectKey=${ProjectKey} -X'''
+        sh ''' ${SCANNER_HOME}/bin/sonar_scanner -Dsonar.projectName=${ProjectName} -Dsonar.projectKey=${ProjectKey} -X'''
     }
 }
